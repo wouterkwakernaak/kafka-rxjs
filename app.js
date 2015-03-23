5613b55c06b0802639e75deb244de067ec9617d8
Wouter Kwakernaak <wouter@wouterkwakernaak.nl>
2015-03-23 14:57:59 +0100

    using kafka event emitter as observable

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var Rx = require('rx');
 var kafka = require('kafka-node');
-var client = new kafka.Client('dockerhost:49153');
+var client = new kafka.Client('dockerhost:2181');
 consumer = new kafka.HighLevelConsumer(client, [{ topic: 'rxjs' }], { groupId: 'rxjs-consumers' });
 
 consumer.on('error', function(err) {
 	console.log(err);
 });
-consumer.on('message', function(message) {
-	console.log(message);
+
+var source = Rx.Observable.fromEvent(consumer, 'message')
+var subscription = source.subscribe(function (message) {
+  console.log(message);
 });

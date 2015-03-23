464aa4460a1e721bb91cc23c7b907544ad250301
Wouter Kwakernaak <wouter@wouterkwakernaak.nl>
2015-03-23 14:24:04 +0100

    basic kafka consumer

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var Rx = require('rx');
 var kafka = require('kafka-node');
+var client = new kafka.Client('dockerhost:49153');
+consumer = new kafka.HighLevelConsumer(client, [{ topic: 'rxjs' }], { groupId: 'rxjs-consumers' });
+
+consumer.on('error', function(err) {
+	console.log(err);
+});
+consumer.on('message', function(message) {
+	console.log(message);
+});

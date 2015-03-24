6d92ce918145ec233f666c1aa8f46f5972d348a1
Wouter Kwakernaak <wouter@wouterkwakernaak.nl>
2015-03-24 13:50:29 +0100

    added a commented out fake source for testing

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ consumer.on('error', function(err) {
 	console.log(err);
 });
 
+// var array = [{value: 'x'}, {value: 'y'}];
+// var source = Rx.Observable.interval(2000)
+// 				.take(array.length)
+// 				.map(function(i) {
+// 					return array[i];
+// 				})
+
 var source = Rx.Observable.fromEvent(consumer, 'message')
 				.map(function(message) {
 					return message.value;
 				})
-				.scan('start', function(acc, x) {
+				.scan(function(acc, x) {
 					return acc + x;
 				});
 
 var subscription = source.subscribe(function (message) {
-  console.log(message);
+	console.log(message);
 });
